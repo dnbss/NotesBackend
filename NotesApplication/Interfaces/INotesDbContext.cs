5cc2bdea740c94ad9de4bc5f43ca6862c8c8506e
dnbss <muhomossyas@gmail.com>
2022-06-26 17:05:03 +0300

    add db context

---
+++ NotesApplication/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesDomain;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NotesApplication.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
