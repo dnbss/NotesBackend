358e9f44b245cf5d62da00884ac33d537977c92d
dnbss <muhomossyas@gmail.com>
2022-06-26 15:30:01 +0300

    Add project files.

---
+++ NotesDomain/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesDomain
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Edited { get; set; }
+
+    }
+}
