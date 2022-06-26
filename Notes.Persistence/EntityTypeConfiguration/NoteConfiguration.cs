ec6e9a6319591480ab458be1b6d8c5ea338d1ad5
dnbss <muhomossyas@gmail.com>
2022-06-26 16:40:04 +0300

    create note db configure

---
+++ Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NotesDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence.EntityTypeConfiguration
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+
+            builder.HasIndex(note => note.Id).IsUnique();
+
+            builder.Property(note => note.Title).HasMaxLength(250); 
+        }
+    }
+}
